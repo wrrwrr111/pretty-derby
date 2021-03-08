c8c707067faf3840f5cde1242529bd4d3deef601
baze <wrrwrr111@gmail.com>
2021-03-08 18:28:12 +0800

    delete build

---
+++ src/seed.js
@@ -0,0 +1,10 @@
+import React,{useState} from 'react';
+import { Divider,Row,Popover,Image,Button,Checkbox,Modal,PageHeader} from 'antd';
+
+import axios from 'axios'
+
+const Seed = ()=>{
+
+  return(<></>)
+}
+export default Seed
