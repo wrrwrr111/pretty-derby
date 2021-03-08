faca5aee9ed46aa5d6cbe60d2d874fbd005aec79
baze <wrrwrr111@gmail.com>
2021-03-08 20:41:52 +0800

    update build

--- config-overrides.js
+++ config-overrides.js
@@ -9,8 +9,8 @@ module.exports = override(
     const paths = require('react-scripts/config/paths');
 
     // 修改public path to github cdn
-    paths.publicUrl = 'https://cdn.jsdelivr.net/gh/wrrwrr111/pretty-derby@gh-pages/'
-    config.output.publicPath = 'https://cdn.jsdelivr.net/gh/wrrwrr111/pretty-derby@gh-pages/'
+    paths.publicUrl = 'https://cdn.jsdelivr.net/gh/wrrwrr111/pretty-derby/public/'
+    config.output.publicPath = 'https://cdn.jsdelivr.net/gh/wrrwrr111/pretty-derby/public/'
     return config
   }
 )
