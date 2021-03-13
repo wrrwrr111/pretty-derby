bac4e328fcf3c01c9b7bda5926345e989ae44cdc
baze <wrrwrr111@gmail.com>
2021-03-13 23:46:03 +0800

    拆分翻译文件

--- src/components/t.js
+++ src/components/t.js
@@ -1,8 +1,19 @@
-import db from '../db.js'
+import low from 'lowdb';
+import LocalStorage from 'lowdb/adapters/LocalStorage'
 
-const lan = db.get('lan').value()
-const zh = db.get(lan||'zh').value()
+import ZH from '../assert/zh.json'
+import EN from '../assert/en.json'
+const adapter = new LocalStorage('db')
+const db = low(adapter)
+
+const lan = db.get('lan').value()||'zh'
+let lanData = {}
+if(lan==='zh'){
+  lanData = ZH.zh
+}else{
+  lanData = EN.en
+}
 const t = (text)=>{
-    return zh[text] || text
+    return lanData[text] || text
 }
 export  default t
