eba2102353b7cc204dbc9fb0798b40f11118b240
baze <wrrwrr111@gmail.com>
2021-04-24 04:23:20 +0800

    重写技能详情

--- src/components/skill-detail.js
+++ src/components/skill-detail.js
@@ -99,18 +99,20 @@ const SkillDetail = (props)=>{
     return flag
   })
 
-  return <div style={{maxWidth:600,textAlign:'left',overflowWrap:'break-word'}}>
+  return <div style={{maxWidth:600,textAlign:'left',
+    overflowWrap:'break-word',wordBreak:'break-all'}}>
   <Image src={cdnServer+skill.imgUrl} preview={false} width={52}></Image>
-  <p>{t('技能名称')+ ':  ' +t(skill.name)}</p>
-  <p>{t('技能描述')+ ':  ' +skill.describe}</p>
-  <p>{t('技能描述')+ ':  ' +t(skill.describe)}</p>
-  <p>{t('触发条件')+ ':  ' +skill.condition}</p>
-  <p>{t('触发条件')+ ':  ' +t(skill.condition)}</p>
-  <p>{`${t('技能效果')}:\xa0\xa0 ${skill.ability.map(ability=>skillType[ability.type]+' '+ability.value/10000)}`}</p>
+  <p>{`${t('技能名称')}:\xa0\xa0${skill.name}`}</p>
+  <p>{`${t('技能名称')}:\xa0\xa0${t(skill.name)}`}</p>
+  <p>{`${t('技能描述')}:\xa0\xa0${skill.describe}`}</p>
+  <p>{`${t('技能描述')}:\xa0\xa0${t(skill.describe)}`}</p>
+  <p>{`${t('触发条件')}:\xa0\xa0${skill.condition}`}</p>
+  <p>{`${t('触发条件')}:\xa0\xa0${t(skill.condition)}`}</p>
+  <p>{`${t('技能效果')}:\xa0\xa0${skill.ability.map(ability=>skillType[ability.type]+' '+ability.value/10000)}`}</p>
   <p>{`${t('持续时间')}:\xa0\xa0${skill.ability_time/10000}s*${t('赛道长度')}/1000`}</p>
   <p>{`${t('冷却时间')}:\xa0\xa0${skill.cooldown/10000}s*${t('赛道长度')}/1000`}</p>
   <p>{`${t('技能价格')}:\xa0\xa0${skill.need_skill_point}\xa0Pt`}</p>
-  <p>{`${t('评分')}:\xa0${skill.grade_value}`}</p>
+  <p>{`${t('技能评分')}:\xa0\xa0${skill.grade_value}`}</p>
   {!isNur&&<>
     <Divider>{t('支援卡')}</Divider>
     <Row>
