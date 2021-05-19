14e66c8b49522ce83aa4a544290d1464b6251d25
baze <wrrwrr111@gmail.com>
2021-05-19 22:39:58 +0800

    update build

--- src/components/skill-detail.js
+++ src/components/skill-detail.js
@@ -99,7 +99,8 @@ const SkillDetail = (props)=>{
     return flag
   })
 
-  return <div style={{maxWidth:600,textAlign:'left',
+  return <div style={{maxWidth:600,maxHeight:window.innerHeight-104,
+    overflow:'auto',textAlign:'left',
     overflowWrap:'break-word',wordBreak:'break-all'}}>
   <Image src={cdnServer+skill.imgUrl} preview={false} width={52}></Image>
   <p>{`${t('技能名称')}:\xa0\xa0${skill.name}`}</p>
