74fa66e2040f51b9437b28bd52375a0e6d38cd4f
wrrwrr111 <wrrwrr111@gmail.com>
2022-04-28 16:34:14 +0800

    重构翻译

--- src/components/t.js
+++ src/components/t.js
@@ -1,31 +1,26 @@
-import low from 'lowdb';
-import LocalStorage from 'lowdb/adapters/LocalStorage'
+import low from "lowdb";
+import LocalStorage from "lowdb/adapters/LocalStorage";
 
-import ZH from '../assert/cn.json'
-import EN from '../assert/us.json'
-import JP from '../assert/jp.json'
-const adapter = new LocalStorage('db')
-const db = low(adapter)
+import ZH from "../assert/locales/zh_CN.json";
+import EN from "../assert/locales/en.json";
+const adapter = new LocalStorage("db");
+const db = low(adapter);
 
-const lan = db.get('lan').value() || 'cn'
-let lanData = {}
-if (lan === 'cn') {
-  lanData = ZH
-} else if (lan === 'us') {
-  lanData = EN
-} else if (lan === 'jp') {
-  lanData = JP
+const lan = db.get("lan").value() || "cn";
+let lanData = {};
+if (lan === "cn") {
+  lanData = ZH;
+} else if (lan === "us") {
+  lanData = EN;
 }
 const t = (text, lan) => {
-  if (lan === 'cn') {
-    lanData = ZH
-  } else if (lan === 'us') {
-    lanData = EN
-  } else if (lan === 'jp') {
-    lanData = JP
+  if (lan === "cn") {
+    lanData = ZH;
+  } else if (lan === "us") {
+    lanData = EN;
   }
-  let result = lanData[text] || text
+  let result = lanData[text] || text;
 
-  return result
-}
-export default t
+  return result;
+};
+export default t;
