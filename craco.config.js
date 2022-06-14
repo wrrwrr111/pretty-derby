fa34b3650314920cc97caa9f9388cadbfb052852
wrrwrr111 <wrrwrr111@gmail.com>
2022-06-14 17:48:33 +0800

    build: craco配置

--- craco.config.js
+++ craco.config.js
@@ -6,6 +6,29 @@ module.exports = {
       plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      if (process.env.NODE_ENV === "development") {
+        return webpackConfig;
+      }
+      webpackConfig.output = {
+        ...webpackConfig.output,
+        publicPath: "https://cdn.jsdelivr.net/gh/wrrwrr111/pretty-derby@master/build/",
+      };
+      return webpackConfig;
+    },
+  },
+  babel: {
+    plugins: [
+      [
+        "import",
+        {
+          libraryName: "antd-mobile",
+          style: "css",
+        },
+      ],
+    ],
+  },
   plugins: [
     {
       plugin: CracoAlias,
