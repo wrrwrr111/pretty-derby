54e0c1efea96e4121f100144a0e47827b7c5133d
wrrwrr111 <wrrwrr111@gmail.com>
2022-06-14 16:24:53 +0800

    ci: 添加purge脚本

---
+++ purge.sh
@@ -0,0 +1,3 @@
+curl https://purge.jsdelivr.net/gh/wrrwrr111/pretty-derby@master/src/assert/db.json
+curl https://purge.jsdelivr.net/gh/wrrwrr111/pretty-derby@master/src/assert/locale/zh_CN.json
+curl https://purge.jsdelivr.net/gh/wrrwrr111/pretty-derby@master/src/assert/locale/en.json
