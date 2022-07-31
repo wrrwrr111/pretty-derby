405b5771794a771044e04bfd4895ee78c9441592
wrrwrr111 <wrrwrr111@gmail.com>
2022-07-31 23:19:02 +0800

    Revert "Revert "Merge branch 'master' into build2""
    This reverts commit 041eb1615eccf1b822cf27cbd32c27f525a84079.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "antd/dist/antd.css";
-// import 'antd-mobile/dist/antd-mobile.min.css';
 import "@material-tailwind/react/tailwind.css";
-import "./styles/tailwind.css";
+import "./styles/index.css";
 import App from "./app";
 import "./i18n";
 
