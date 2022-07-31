ff2dc2d9989959e843baa5354d78738a64814790
wrrwrr111 <wrrwrr111@gmail.com>
2022-07-31 23:36:58 +0800

    update build

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import "antd/dist/antd.css";
-import "@material-tailwind/react/tailwind.css";
 import "./styles/index.css";
+import "antd/dist/antd.css";
 import App from "./app";
 import "./i18n";
+import "@material-tailwind/react/tailwind.css";
 
 const Index = () => {
   return (
