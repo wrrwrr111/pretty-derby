b7a13cd9b05a04f52f44ab88aec3e41aca6fe124
wrrwrr111 <wrrwrr111@gmail.com>
2023-01-16 16:43:24 +0800

    Update next.config.js

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const { i18n } = require("./next-i18next.config");
-const withTM = require("next-transpile-modules")(["@mui/x-data-grid"]);
-
-module.exports = withTM({
-  /* Your Next.js config */
-});
