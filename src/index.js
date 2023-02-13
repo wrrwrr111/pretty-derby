81c1b71284e21599a7e1a164daf0d97fbc394950
wrrwrr111 <wrrwrr111@gmail.com>
2023-02-13 15:12:50 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import "antd/dist/antd.css";
+import "@material-tailwind/react/tailwind.css";
+import "./styles/index.css";
 import App from "./app";
 import "./i18n";
-
-import "antd/dist/antd.css";
-import "./styles/tailwind.css";
-import "./styles/index.css";
 
 const Index = () => {
   return (
