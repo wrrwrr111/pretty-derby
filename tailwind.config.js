96680bc8fae050717931cdf6e89b0716b8399a1d
wrrwrr111 <wrrwrr111@gmail.com>
2025-04-01 18:32:29 +0800

    remove daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const daisyui = require("daisyui");
 const typography = require("@tailwindcss/typography");
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [typography, daisyui],
+  plugins: [typography],
 };
